e950e138c82075a10c55811fde65771534611ac0
dbqlsgovl <dbqlsgovl_@naver.com>
2021-05-06 21:59:57 +0900

    search

---
+++ html/api_req.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(int argc, char ** argv)
+{	//function that get the user input from search.php, and return URL for restAPI
+	//substitute : chars have to be replaced with its hexadecimal ASCII code for using API
+ 	//pre_query : restAPI setting URL before query
+	//searchString : the user input
+	//query_len : expected length of current URL
+	//query : current URL
+	//idx : index of searchString in which the currently found special character is
+	char *substitute = " `@#$%^&=+[{]}\\|;:\",<>/?";
+	char *pre_query = "https://unogsng.p.rapidapi.com/search?orderby=relevance&countrylist=348&query=";
+	char searchString[strlen(argv[1]) + 1];
+	int query_len = (strlen(pre_query) + strlen(searchString) + 1);
+	char *query = (char *)malloc(query_len * sizeof(char));
+	int idx;
+	
+	sprintf(searchString, argv[1]);
+	strcpy(query, pre_query);
+	
+	//this for statement replace the special characters in searchString to ASCII code then assigned the changed string to query
+	//i is index of searchString where the comparing start
+	for( int i = 0; i < strlen(searchString); i = idx + 1)
+	{
+		idx = i + strcspn( &searchString[i], substitute);
+
+		query_len += 2;	//replacing @ with %40 extends the length 2 more
+		query = realloc(query, query_len * sizeof(char));
+
+		strncat(query, &searchString[i], (idx - i));
+		if ( idx < strlen(searchString)) {
+			sprintf( query, "%s%%%x", query, searchString[idx]);
+		}
+	}
+	printf("%s", query);
+	free(query);
+	return 0;
+}
---
+++ html/search.php
@@ -0,0 +1,54 @@
+<?php
+	include 'check_login.php';
+
+    //get user input from searchWin.php and make URL for restAPI
+	$searchString = $_POST["movie_php"];
+	$query = shell_exec("./api_req \"$searchString\"");
+
+	echo "query is \n";
+	print_r($query);
+
+    //start cURL request to restAPI
+	$curl = curl_init();
+
+	curl_setopt_array($curl, [
+        CURLOPT_URL => $query,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => [
+            "x-rapidapi-host: unogsng.p.rapidapi.com",
+            "x-rapidapi-key: c84d0d4f12mshfd3aef6215c4f30p1db145jsn5312b6966b63"
+        ],
+	]);
+    
+    //send request
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
+
+	if ($err) {
+		$curl_data = NULL;
+		echo "cURL Error #:".$err;
+		curl_close($curl);
+	} else {
+		$curl_data = $response;
+		curl_close($curl);
+
+        //start to write json data to file
+		$filename = "searchResult.json";
+		$fp = fopen($filename, "w") or die("Unable to open file!");
+		$arr_data = json_decode($curl_data, true);
+		$json_data = json_encode($arr_data);
+		$bytes = file_put_contents($filename, $json_data);
+
+		echo $bytes;
+		echo "done";
+
+        //move to next web page
+		echo ("<script>location.href='searchResult.php';</script>");  
+	}
+?>
